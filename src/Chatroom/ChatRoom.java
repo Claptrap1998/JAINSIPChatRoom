97c9c0d2b0c16e118d053dfcb1f3cca9b8823795
youqie <youqie@vip.qq.com>
2020-06-12 08:59:46 +0800

    Fix bugs

--- src/Chatroom/ChatRoom.java
+++ src/Chatroom/ChatRoom.java
@@ -3,51 +3,49 @@ package Chatroom;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
-import javax.swing.*;
 import java.awt.Color;
 import java.io.*;
 import java.net.InetAddress;
-import java.net.Socket;
-import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class ChatRoom extends Application implements MessageProcessor{
 
+    private static String username;
+    private static SipLayer sipLayer;
+    private static final String SERVERHOST = "172.17.24.113";
+    private static final int SERVERPORT = 5061;
+//    private static final String SERVERHOST = "39.96.223.157";
+//    private static final int SERVERPORT = 7654;
+
+    static FXMLLoader fxmlLoader;
+    static GridPane root;
+    static ChatController controller;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ChatRoomGUI.fxml"));
-        GridPane root = fxmlLoader.load();
-        ChatController controller = fxmlLoader.getController();
         Scene scene = new Scene(root);
-
-//        //获取参数
-//        List<String> parameters = getParameters().getUnnamed();
-//        if(parameters.size()!=1){
-//            System.out.println("Run Configuration Parameters: MyName");
-//            System.exit(-1);
-//        }
-//        String  myName   = parameters.get(0); //我的名字
-
+        System.out.println("start method");
+        //发送按钮
         controller.sendButton.setOnAction(e->{
-//            jButton1ActionPerformed();
             // TODO add your handling code here:
             try{
                 String dest_name = controller.nameToSend.getText();
                 String message = controller.msgToSend.getText();
 
+                System.out.println(dest_name + message);
                 String serverTo = "sip:" + "SERVER" + "@" + SERVERHOST + ":" + SERVERPORT;
-                siplayer.sendMessage(serverTo, "MESSAGE:" + dest_name + "\n" + username + "@" + message);
+                System.out.println(serverTo);
+                sipLayer.sendMessage(serverTo, "MESSAGE:" + dest_name + "\n" + username + "@" + message);
             } catch (Throwable a)
             {
                 a.printStackTrace();
-                insertDocument1("ERROR sending message: " + a.getMessage() + "\n", Color.red);
+                printMessage("ERROR sending message: " + a.getMessage() + "\n");
             }
         });
 
@@ -67,7 +65,7 @@ public class ChatRoom extends Application implements MessageProcessor{
             System.exit(0);
         });
         //显示GUI界面
-        primaryStage.setTitle("ChatRoom: " );
+        primaryStage.setTitle("ChatRoom" );
 
         //设定窗口无边框
         primaryStage.initStyle(StageStyle.UNDECORATED);
@@ -88,58 +86,30 @@ public class ChatRoom extends Application implements MessageProcessor{
         primaryStage.show();
     }
 
-    private void jButton1ActionPerformed() {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ChatRoomGUI.fxml"));
-        ChatController controller = fxmlLoader.getController();
-        // TODO add your handling code here:
-        try{
-            String dest_name = controller.nameToSend.getText();
-            String message = controller.msgToSend.getText();
-
-            String serverTo = "sip:" + "SERVER" + "@" + SERVERHOST + ":" + SERVERPORT;
-            siplayer.sendMessage(serverTo, "MESSAGE:" + dest_name + "\n" + username + "@" + message);
-        } catch (Throwable e)
-        {
-            e.printStackTrace();
-            insertDocument1("ERROR sending message: " + e.getMessage() + "\n", Color.red);
-        }
-    }
-
-    public void insertDocument1(String text, Color textColor)//根据传入的颜色及文字，将文字插入文本域
+    public void printMessage(String text)//根据传入的颜色及文字，将文字插入文本域
     {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ChatRoomGUI.fxml"));
-        ChatController controller = fxmlLoader.getController();
-        controller.msgToDisplay.setText(text);
-    }
-
-    public void insertDocument2(String text, Color textColor)//根据传入的颜色及文字，将文字插入文本域
-    {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ChatRoomGUI.fxml"));
-
-        ChatController controller = fxmlLoader.getController();
         controller.msgToDisplay.appendText(text);
     }
-    public ChatRoom()
+
+    public void printUserList(String text)//根据传入的颜色及文字，将文字插入文本域
     {
-        super();
-        try {
-            UIManager.setLookAndFeel(UIManager
-                    .getSystemLookAndFeelClassName());
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-//        String from = "sip:" + sip.getUsername() + "@" + sip.getHost() + ":" + sip.getPort();
-//        this.jLabel3.setText(sip.getUsername());
-//        this.jLabel5.setText(sip.getHost());
-//        this.jLabel7.setText(""+sip.getPort());
+        controller.data1.setText(text);
     }
+
+    public ChatRoom() throws IOException {
+        fxmlLoader = new FXMLLoader(ChatRoom.class.getResource("ChatRoomGUI.fxml"));
+        root = fxmlLoader.load();
+        controller = fxmlLoader.getController();
+        System.out.println("无参构造");
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception{
+
         if(args.length != 2) {
-            printUsage();
+            System.out.println("请输入参数！");
             System.exit(-1);
         }
 
@@ -149,83 +119,73 @@ public class ChatRoom extends Application implements MessageProcessor{
             int port = Integer.parseInt(args[1]);
             String ip = InetAddress.getLocalHost().getHostAddress();
 
-            siplayer = new SipLayer(username, ip, port);
-//            jainSipClient jsc = new jainSipClient(sipLayer);
-//            sipLayer.setMessageProcessor(jsc);
-            ChatRoom jr = new ChatRoom();
-            jr.sipLayer = siplayer;
-            siplayer.setMessageProcessor(jr);
-//            jsc.show();
-
+            sipLayer = new SipLayer(username, ip, port);
+            ChatRoom jr = new ChatRoom(sipLayer);
+            sipLayer.setMessageProcessor(jr);
             String clientMessage = "CLIENTINFO:" + username + "@" + ip + ":" + port;
             String to = "sip:" + "SERVER" + "@" + SERVERHOST + ":" + SERVERPORT;
-            siplayer.sendMessage(to, clientMessage);
+            sipLayer.sendMessage(to, clientMessage);
         }catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+
         launch(args);
     }
 
-    private static void printUsage()
-    {
-        System.out.println("Syntax:");
-        System.out.println("  java -jar textclient.jar <username> <port>");
-        System.out.println("where <username> is the nickname of this user");
-        System.out.println("and <port> is the port number to use. Usually 5060 if not used by another process.");
-        System.out.println("Example:");
-        System.out.println("  java -jar textclient.jar snoopy71 5061");
+
+    public ChatRoom(SipLayer sip) throws IOException {
+        super();
+        System.out.println("含参构造");
+        sipLayer = sip;
+        System.out.println("初始化sipLayer完成");
+        fxmlLoader = new FXMLLoader(ChatRoom.class.getResource("ChatRoomGUI.fxml"));
+        root = fxmlLoader.load();
+        controller = fxmlLoader.getController();
     }
 
-
-
-    private static String username = "alice";
-    public static SipLayer sipLayer;
-    public static SipLayer siplayer;
-    private static final String SERVERHOST = "192.168.0.103";
-    private static final int SERVERPORT = 8848;
     // End of variables declaration//GEN-END:variables
     @Override
     public void processError(String errorMessage) {
         // TODO Auto-generated method stub
-        insertDocument1("ERROR: " + errorMessage + "\n", Color.red);
+        System.out.println("处理错误");
+        printMessage("ERROR: " + errorMessage + "\n");
     }
 
     @Override
     public void processInfo(String infoMessage) {
         // TODO Auto-generated method stub
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ChatRoomGUI.fxml"));
-        ChatController controller = fxmlLoader.getController();
+
         if(infoMessage.equals("WHISPER")){
             System.out.println("111111111111111");
-            insertDocument1("To " + controller.nameToSend.getText() + ": " + controller.msgToSend.getText() + "\n", Color.magenta);
+            printMessage("To " + controller.nameToSend.getText() + ": " + controller.msgToSend.getText() + "\n");
             //controller.msgToDisplay.setCaretPosition(jTextPane1.getDocument().getLength());
             controller.msgToSend.setText("");
         }else if(infoMessage.equals("GROUP")){
-            insertDocument1("To " + "ALL" + ": " + controller.msgToSend.getText() + "\n", Color.black);
+            System.out.println("处理群聊");
+            printMessage("To " + "ALL" + ": " + controller.msgToSend.getText() + "\n");
             //jTextPane1.setCaretPosition(jTextPane1.getDocument().getLength());
             controller.msgToSend.setText("");
         }else {
-			/*insertDocument1("已连接服务器\n", Color.black);
-			jTextPane1.setCaretPosition(jTextPane1.getDocument().getLength());
-			*/
+            System.out.println("处理用户信息");
             controller.msgToSend.setText("");
-            insertDocument2(infoMessage, Color.green);
-            //jTextPane2.setCaretPosition(jTextPane2.getDocument().getLength());
+            printUserList(infoMessage);
         }
     }
 
     @Override
     public void processWhisperMessage(String sender, String message) {
         // TODO Auto-generated method stub
-        insertDocument1("From " + sender + ": " + message + "\n", Color.magenta);
+        System.out.println("处理私聊-信息");
+        printMessage("From " + sender + ": " + message + "\n");
         //jTextPane1.setCaretPosition(jTextPane1.getDocument().getLength());
     }
 
     @Override
     public void processGroupMessage(String sender, String message) {
         // TODO Auto-generated method stub
-        insertDocument1("From " + sender + ": " + message + "\n", Color.black);
+        System.out.println("处理群聊-信息");
+        printMessage("From " + sender + ": " + message + "\n");
         //jTextPane1.setCaretPosition(jTextPane1.getDocument().getLength());
     }
 
